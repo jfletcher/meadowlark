98184e73ecc9ed745dbbab3c23814232f8b469ea
Jamie Fletcher <jamiemfletcher@gmail.com>
2016-06-30 14:52:19 +0100

    Add Mocha and Chai test suites

--- meadowlark.js
+++ meadowlark.js
@@ -12,6 +12,11 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+// test setup
+app.use(function(req, res, next) {
+  res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+});
+
 // Add routes
 app.get('/', function(req, res) {
   res.render('home');
